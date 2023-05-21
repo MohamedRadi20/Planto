02281fe7b5a3ed82ba3280497d4a6afef1530469
hot-dog <mohamedmm99879@gmail.com>
2023-05-21 10:41:40 +0300

    Integrated the current weather data feature of OpenWeatherMap API and prepared for Geocoding API integration.

--- app/src/main/java/com/example/planto/Home_Fragment.java
+++ app/src/main/java/com/example/planto/Home_Fragment.java
@@ -1,5 +1,12 @@
 package com.example.planto;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,9 +15,21 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+
+import cz.msebera.android.httpclient.Header;
+
+import com.bumptech.glide.Glide;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
 
 import pl.droidsonroids.gif.GifImageView;
 
@@ -19,6 +38,24 @@ import pl.droidsonroids.gif.GifImageView;
  * create an instance of this fragment.
  */
 public class Home_Fragment extends Fragment {
+    final String APP_ID = "f88cf7dd4b8bce0b2247e84e8a71a6ee";
+    final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+    final long MIN_TIME = 5000;
+    final float MIN_DISTANCE = 1000;
+    final int REQUEST_CODE = 101;
+
+    String Location_Provider = LocationManager.GPS_PROVIDER;
+    Animation left, right, fade_in;
+
+    TextView NameofCity, weatherState, Temperature, wind_speed, humidity;
+    ImageView mweatherIcon;
+
+
+    LocationManager mLocationManager;
+    LocationListener mLocationListner;
+    CardView card_view_1, card_view_2, card_view_3, card_view_4, mCityFinder;
+    TextView name;
 
 
     @Override
@@ -31,27 +68,172 @@ public class Home_Fragment extends Fragment {
 
     public void onStart() {
         super.onStart();
-        TextView name;
-        CardView card_view_1, card_view_2, card_view_3, card_view_4;
-        GifImageView home_sun;
-        Animation left, right, down;
 
         card_view_1 = getActivity().findViewById(R.id.card_view_1);
         card_view_2 = getActivity().findViewById(R.id.card_view_2);
         card_view_3 = getActivity().findViewById(R.id.card_view_3);
         card_view_4 = getActivity().findViewById(R.id.card_view_4);
 
-        down = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.down);
+        weatherState = getActivity().findViewById(R.id.weatherCondition);
+        Temperature = getActivity().findViewById(R.id.temperature);
+        mweatherIcon = getActivity().findViewById(R.id.weatherIcon);
+        mCityFinder = getActivity().findViewById(R.id.cityFinder);
+        NameofCity = getActivity().findViewById(R.id.cityName);
+        wind_speed = getActivity().findViewById(R.id.wind_speed);
+        humidity = getActivity().findViewById(R.id.humidity);
+
         left = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.left);
         right = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.right);
+        fade_in = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.fade_in);
 
+        mCityFinder.setAnimation(fade_in);
         card_view_1.setAnimation(left);
         card_view_2.setAnimation(right);
         card_view_3.setAnimation(left);
         card_view_4.setAnimation(right);
 
-        name = (TextView) getActivity().findViewById(R.id.name);
+        name = getActivity().findViewById(R.id.name);
         name.setText("Straw Hats");
 
+        //TODO create the cityFinder activity
+
+//        mCityFinder.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity().getApplicationContext(), cityFinder.class);
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Intent mIntent = getActivity().getIntent();
+        String city = mIntent.getStringExtra("City");
+        if (city != null) {
+            getWeatherForNewCity(city);
+        } else {
+            getWeatherForCurrentLocation();
+        }
+    }
+
+
+    private void getWeatherForNewCity(String city) {
+        RequestParams params = new RequestParams();
+        params.put("q", city);
+        params.put("appid", APP_ID);
+        letsdoSomeNetworking(params);
+
+    }
+
+
+    private void getWeatherForCurrentLocation() {
+
+        mLocationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        mLocationListner = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                String Latitude = String.valueOf(location.getLatitude());
+                String Longitude = String.valueOf(location.getLongitude());
+
+                RequestParams params = new RequestParams();
+                params.put("lat", Latitude);
+                params.put("lon", Longitude);
+                params.put("appid", APP_ID);
+                letsdoSomeNetworking(params);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                //not able to get location
+            }
+        };
+
+
+        if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        mLocationManager.requestLocationUpdates(Location_Provider, MIN_TIME, MIN_DISTANCE, mLocationListner);
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getActivity().getApplicationContext(), "Locationget Succesffully", Toast.LENGTH_SHORT).show();
+                getWeatherForCurrentLocation();
+            } else {
+                //user denied the permission
+            }
+        }
+
+
+    }
+
+
+    private void letsdoSomeNetworking(RequestParams params) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+                Toast.makeText(getActivity().getApplicationContext(), "Data Get Success", Toast.LENGTH_SHORT).show();
+
+                weatherData weatherD = weatherData.fromJson(response);
+                updateUI(weatherD);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+            }
+        });
+
+
+    }
+
+    private void updateUI(weatherData weather) {
+
+        Temperature.setText(weather.getmTemperature());
+        NameofCity.setText(weather.getMcity());
+        weatherState.setText(weather.getmWeatherType());
+        String imageUrl = weather.getMicon();
+
+        Glide.with(getActivity().getApplicationContext())
+                .load(imageUrl)
+                .into(mweatherIcon);
+
+        wind_speed.setText(weather.wind_speed());
+        humidity.setText(weather.humidity());
+
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mLocationManager != null) {
+            mLocationManager.removeUpdates(mLocationListner);
+        }
     }
 }
---
+++ app/src/main/java/com/example/planto/weatherData.java
@@ -0,0 +1,79 @@
+package com.example.planto;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class weatherData {
+
+    private String mTemperature, micon, mcity, mWeatherType, mWeather_icon_Type, humidity, wind_speed;
+    private int mCondition;
+
+    public static weatherData fromJson(JSONObject jsonObject) {
+
+        try {
+            weatherData weatherD = new weatherData();
+            weatherD.mcity = jsonObject.getString("name");
+            weatherD.mCondition = jsonObject.getJSONArray("weather").getJSONObject(0).getInt("id");
+            weatherD.mWeatherType = jsonObject.getJSONArray("weather").getJSONObject(0).getString("main");
+
+            weatherD.mWeather_icon_Type = jsonObject.getJSONArray("weather").getJSONObject(0).getString("icon");
+            weatherD.micon = updateWeatherIcon(weatherD.mWeather_icon_Type);
+
+            Log.e("fuck me pal: ", "yo");
+
+            double humidityResult = jsonObject.getJSONObject("main").getDouble("humidity");
+            int humidity_roundedValue = (int) Math.rint(humidityResult);
+            weatherD.humidity = Integer.toString(humidity_roundedValue);
+            Log.e("fuck me pal: ", weatherD.humidity);
+
+            double windResult = jsonObject.getJSONObject("wind").getDouble("speed");
+            int wind_roundedValue = (int) Math.rint(windResult);
+            weatherD.wind_speed = Integer.toString(wind_roundedValue);
+            Log.e("fuck me pal: ", weatherD.wind_speed);
+
+
+            double tempResult = jsonObject.getJSONObject("main").getDouble("temp") - 273.15;
+            int temp_roundedValue = (int) Math.rint(tempResult);
+            weatherD.mTemperature = Integer.toString(temp_roundedValue);
+            return weatherD;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+
+
+    private static String updateWeatherIcon(String icon) {
+        String imageUrl = "https://openweathermap.org/img/w/" + icon + ".png";
+        return imageUrl;
+    }
+
+    public String getmTemperature() {
+        return mTemperature + "°C";
+    }
+
+    public String getMicon() {
+        return micon;
+    }
+
+    public String getMcity() {
+        return mcity;
+    }
+
+    public String getmWeatherType() {
+        return mWeatherType;
+    }
+
+    public String humidity() {
+        return "Humidity: " + humidity + "%";
+    }
+
+    public String wind_speed() {
+        return "Wind: " + wind_speed + " mph";
+    }
+
+}
